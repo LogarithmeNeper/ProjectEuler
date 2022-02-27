570fc2726925599248f2571cd291d5811e02410e
Pierre Sibut-Bourde <pierre.sibut.bourde@gmail.com>
2022-02-27 10:55:52 +0100

    pb2+pb3

--- solving.py
+++ solving.py
@@ -1,8 +1,21 @@
 import utils
 
 def pb1():
-    res = 0
+    result = 0
     for i in range(1000):
         if (i%3 == 0) or (i%5 == 0):
-            res+=i
-    return res
+            result += i
+    return result
+
+def pb2():
+    result = 0
+    f = utils.fibonacci()
+    curr = next(f)
+    while curr < 4000000:
+        if curr%2 == 0:
+            result += curr
+        curr = next(f)
+    return result
+
+def pb3():
+    return utils.prime_factors(600851475143)[-1]
---
+++ utils.py
@@ -0,0 +1,28 @@
+import math
+
+# This function uses a generator to get values of the Fibonacci sequence.
+# f_{n+2} = f_{n+1} + f_n
+# General seed (0,1) given as named parameter for the function.
+def fibonacci(a=0, b=1):
+    while True:
+        yield a
+        a, b = b, a+b
+
+# This function is used to generate prime factors of a number and to return them as a list.
+def prime_factors(n):
+    factors = []
+    # Getting 2^v_2(n)
+    while n%2 == 0:
+        factors.append(2)
+        n /= 2
+    # Getting odd primes. 
+    for curr in range(3, math.floor(math.sqrt(n))+1):
+        while n%curr == 0:
+            factors.append(curr)
+            n /= curr
+    # At this point, means that n is prime.
+    if n>2:
+        factors.append(n)
+    return factors
+
+
